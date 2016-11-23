f06e6104b85296af51df88f881feb22f79a30e43
yy_yank <yy.yank.dev.blog@gmail.com>
2016-11-23 22:34:07 +0900

    first commit

---
+++ mobile/src/main/kotlin/com/github/yyyank/addict/blocker/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.github.yyyank.addict.blocker
+
+import android.app.Activity
+import android.os.Bundle
+
+/**
+ * Created by yy_yank on 2016/11/23.
+ */
+class MainActivity : Activity(){
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
