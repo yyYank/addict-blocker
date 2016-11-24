487a4f86d81213b615d15e59ad5fd9e52149b8a8
yy_yank <yy.yank.dev.blog@gmail.com>
2016-11-25 01:03:54 +0900

    modify

--- mobile/src/main/kotlin/com/github/yyyank/addict/blocker/activity/MainActivity.kt
+++ mobile/src/main/kotlin/com/github/yyyank/addict/blocker/activity/MainActivity.kt
@@ -2,6 +2,7 @@ package com.github.yyyank.addict.blocker.activity
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.Toast
 
 /**
  * Created by yy_yank on 2016/11/23.
@@ -10,6 +11,7 @@ class MainActivity : Activity(){
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Toast.makeText(this, "Hello My App!!!", Toast.LENGTH_LONG).show()
         super.onCreate(savedInstanceState)
     }
 
